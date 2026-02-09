6a80e5d47fd358e31edb3cd52e719e15f7dcff6e
H4elme <atmrosh@gmail.com>
2026-02-09 12:42:57 +0100

    add progress bar and change spinboxes to label after pressing START button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,14 +5,21 @@
 #include <QPushButton>
 #include <QTimer>
 #include <QDebug>
+#include <QProgressBar>
+#include <QStackedWidget>
+#include <QLabel>
 
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent)
 {
-    this->resize(1000, 1000);
+    this->resize(720, 720);
 
     auto outer = new QVBoxLayout(this);
-    auto top = new QHBoxLayout();
+    auto inputPage = new QWidget();
+    auto timerPage = new QWidget();
+
+    auto inputLayout = new QHBoxLayout(inputPage);
+    auto timerLayout = new QVBoxLayout(timerPage);
 
     seconds = new QSpinBox();
     minutes = new QSpinBox();
@@ -25,9 +32,9 @@ MainWindow::MainWindow(QWidget *parent)
     minutes->setWrapping(true);
     hours->setWrapping(true);
 
-    top->addWidget(hours);
-    top->addWidget(minutes);
-    top->addWidget(seconds);
+    inputLayout->addWidget(hours);
+    inputLayout->addWidget(minutes);
+    inputLayout->addWidget(seconds);
 
     auto startButton = new QPushButton("START");
 
@@ -37,25 +44,68 @@ MainWindow::MainWindow(QWidget *parent)
 
     connect(startButton, &QPushButton::pressed,
             this, &MainWindow::startMyTimer);
-    outer->addLayout(top);
-    outer->addWidget(startButton);
 
+
+    timerLabel = new QLabel();
+
+    timerLabel->setAlignment(Qt::AlignCenter);
+    // move styles to .qss files later?
+    QFont font = timerLabel->font();
+    font.setFamily("Helvetica");
+    font.setPointSize(12);
+    font.setBold(true);
+    timerLabel->setFont(font);
+
+    progressBar = new QProgressBar(this);
+    progressBar->setTextVisible(false);
+    timerLayout->addWidget(timerLabel);
+    timerLayout->addWidget(progressBar);
+
+    // add a class for second page. there just change the pause/continue button, keep everything else the same.
+    stackedWidget = new QStackedWidget(this);
+
+    stackedWidget->addWidget(inputPage);
+    stackedWidget->addWidget(timerPage);
+    outer->addWidget(stackedWidget);
+    outer->addWidget(startButton);
     this->setLayout(outer);
 }
 
 MainWindow::~MainWindow() {}
 
+void MainWindow::updateTimerLabel() {
+    int curHours = totalSeconds / 3600;
+    int curMinutes = (totalSeconds % 3600) / 60;
+    int curSeconds = (totalSeconds % 3600) % 60;
+
+    QString str = QString("%1:%2:%3")
+                      .arg(curHours, 2, 10, QChar('0'))
+                      .arg(curMinutes, 2, 10, QChar('0'))
+                      .arg(curSeconds, 2, 10, QChar('0'));
+    timerLabel->setText(str);
+}
+
 void MainWindow::update() {
     if (totalSeconds) {
         qDebug() << totalSeconds;
         totalSeconds--;
-        hours->setValue(totalSeconds / 3600);
-        minutes->setValue((totalSeconds % 3600) / 60);
-        seconds->setValue((totalSeconds % 3600) % 60);
+        updateTimerLabel();
+        progressBar->setValue(totalSeconds);
+    }
+    else {
+        stackedWidget->setCurrentIndex(0);
     }
 }
 
 void MainWindow::startMyTimer() {
     totalSeconds = hours->text().toInt() * 3600 + minutes->text().toInt() * 60 + seconds->text().toInt();
+    stackedWidget->setCurrentIndex(1);
+    updateTimerLabel();
+    progressBar->setRange(0, totalSeconds);
+    progressBar->setValue(totalSeconds);
     timer->start(1000);
+}
+
+void MainWindow::pauseMyTimer() {
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,9 @@
 #include <QWidget>
 #include <QSpinBox>
 #include <QTimer>
+#include <QProgressBar>
+#include <QLabel>
+#include <QStackedWidget>
 
 class MainWindow : public QWidget
 {
@@ -14,12 +17,17 @@ public:
     bool decrease(QSpinBox*);
     void update();
     void startMyTimer();
+    void pauseMyTimer();
+    void updateTimerLabel();
     ~MainWindow();
 private:
     QSpinBox *seconds;
     QSpinBox *minutes;
     QSpinBox *hours;
     QTimer *timer;
+    QProgressBar *progressBar;
+    QLabel *timerLabel;
+    QStackedWidget *stackedWidget;
     int totalSeconds = 0;
 };
 #endif // MAINWINDOW_H
